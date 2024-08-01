c61b817b8f4fd5c140056dd3b4cce5ea0cf2803b
Marie-Christ Sagbo <christsagbo1@gmail.com>
2024-08-01 21:39:14 +0100

    - finish responsive and add gsap

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import HeroSection from '@/components/HeroSection'
 import Collections from '@/components/Collections'
 import Services from '@/components/Services'
 import CallToAction from '@/components/CallToAction'
-import Footer from '@/components/Footer'
 
 export default function Home () {
   return (
--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,12 +1,26 @@
+'use client'
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 import React from 'react'
 import Image from 'next/image'
 
 export default function HeroSection() {
+  const imageRefs = useRef<HTMLDivElement[]>([]);
+
+  useEffect(() => {
+    const images = imageRefs.current;
+
+    gsap.fromTo(
+      images,
+      { y: 50, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1, stagger: 0.2 }
+    );
+  }, []);
   return (
     <section className="pt-32 pb-20">
-      <div className="flex flex-col lg:flex-row items-center md:items-start space-y-14 md:space-y-0 md:space-x- px-8 lg:px-2">
+      <div className="flex flex-col lg:flex-row items-center md:items-start space-y-14 md:space-y-0 px-8 lg:px-2">
         <div className="flex flex-col space-y-14 md:w-1/2">
-          <h1 className="lg:text-8xl text-6xl text-neutral-800 font-normal">See the NFT <br />new world</h1>
+          <h1 className="lg:text-8xl sm:text-6xl text-5xl text-neutral-800 font-normal">See the NFT <br />new world</h1>
           <p className="text-neutral-400 font-semibold max-w-lg">Vorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eu turpis molestie, di</p>
 
           <div className='flex space-x-4 items-center cursor-pointer'>
@@ -21,7 +35,11 @@ export default function HeroSection() {
         </div>
 
         <div className="relative flex items-center justify-center w-full lg:w-1/2 space-x-4 md:space-x-8">
-          <div className="relative overflow-hidden rounded-full w-[200px] h-[250px] sm:w-[200px] sm:h-[300px] md:w-[250px] md:h-[400px] mt-16 sm:mt-24 md:mt-32">
+          <div 
+          ref={el => {
+            if (el && !imageRefs.current.includes(el)) imageRefs.current.push(el);
+          }}
+          className="relative overflow-hidden rounded-full w-[200px] h-[250px] sm:w-[200px] sm:h-[300px] md:w-[250px] md:h-[400px] mt-16 sm:mt-24 md:mt-32">
             <Image
               src="/images/herosection-image1.svg"
               alt="First Image"
@@ -31,7 +49,11 @@ export default function HeroSection() {
             />
           </div>
 
-          <div className="relative overflow-hidden rounded-full w-[200px] h-[250px] sm:w-[200px] sm:h-[300px] md:w-[250px] md:h-[400px] -mt-16 sm:-mt-24 md:-mt-32">
+          <div 
+           ref={el => {
+            if (el && !imageRefs.current.includes(el)) imageRefs.current.push(el);
+          }}
+          className="relative overflow-hidden rounded-full w-[200px] h-[250px] sm:w-[200px] sm:h-[300px] md:w-[250px] md:h-[400px] -mt-16 sm:-mt-24 md:-mt-32">
             <Image
               src="/images/herosection-image2.svg"
               alt="Second Image"
